27ae41f0beb1851645f79a7a16a59c5cec0d9f5e
Leonardo Ventocilla <ventocilla.leo@gmail.com>
2019-10-13 16:46:26 -0300

    Hero Widget added

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -8,11 +8,54 @@ class WelcomeScreen extends StatefulWidget {
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
 
-class _WelcomeScreenState extends State<WelcomeScreen> {
+class _WelcomeScreenState extends State<WelcomeScreen>
+    with SingleTickerProviderStateMixin {
+
+  AnimationController controller;
+  Animation animation;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(duration: Duration(seconds: 3),vsync: this);
+
+    //animation = CurvedAnimation(parent: controller,curve: Curves.easeIn);
+    animation = ColorTween(begin: Colors.blueGrey, end: Colors.white)
+        .animate(controller);
+
+    //controller.reverse(from: 1);
+    controller.forward();
+
+    /*
+    animation.addStatusListener((status){
+      if(status == AnimationStatus.completed){
+        controller.reverse(from: 1);
+      } else if(status == AnimationStatus.dismissed) {
+        controller.forward();
+      }
+    });
+     */
+
+    controller.addListener((){
+      setState(() {});
+      //print(controller.value);
+      //print(animation.value);
+    });
+
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: animation.value,
+      //backgroundColor: Colors.white,
+      //backgroundColor: Colors.greenAccent.withOpacity(controller.value),
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
@@ -21,12 +64,18 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
           children: <Widget>[
             Row(
               children: <Widget>[
-                Container(
-                  child: Image.asset('images/logo.png'),
-                  height: 60.0,
+                Hero(
+                  tag: 'logo',
+                  child: Container(
+                    child: Image.asset('images/logo.png'),
+                    height: 60.0,
+                    //height: controller.value,
+                    //height: animation.value * 100,
+                  ),
                 ),
                 Text(
                   'Flash Chat',
+                  //'${controller.value.toInt()}%',
                   style: TextStyle(
                     fontSize: 45.0,
                     fontWeight: FontWeight.w900,
